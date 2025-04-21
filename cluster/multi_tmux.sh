6324d89f9961b6fe24ae90c63d7b13bc8068f737
neilhuang <neilhuang@synology.com>
2025-04-21 10:26:15 +0800

    <Script> - support execute command

--- cluster/multi_tmux.sh
+++ cluster/multi_tmux.sh
@@ -12,6 +12,10 @@ PASS="XXX"
 SESSION="ssh_multi"
 SSH_OPTIONS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 
+quote_for_single() {
+	printf "'%s'" "$(printf "%s" "$1" | sed "s/'/'\\\\''/g")"
+}
+
 if ! tmux has-session -t "$SESSION" 2>/dev/null; then
 	tmux new-session -d -s "$SESSION" -n "main" "bash"
 fi
@@ -25,13 +29,13 @@ for ip in "${ip_list[@]}"; do
 
 	if ! tmux list-windows -t "$SESSION" | grep -qE "^\s*[0-9]+:\s+$WIN_NAME\b"; then
 		tmux new-window -t "$SESSION" -n "$WIN_NAME" "$SSH_CMD"
-		tmux send-keys -t "$SESSION:$WIN_NAME" "sleep 1" C-m
 		tmux send-keys -t "$SESSION:$WIN_NAME" "sudo -i" C-m
 		tmux send-keys -t "$SESSION:$WIN_NAME" "$PASS" C-m
 	fi
 
 	if [ -n "$OPTIONAL_COMMAND" ]; then
-		tmux send-keys -t "$SESSION:$WIN_NAME" "$OPTIONAL_COMMAND" C-m
+		SINGLE_QUOTED_CMD=$(quote_for_single "$OPTIONAL_COMMAND")
+		tmux send-keys -t "$SESSION:$WIN_NAME" "bash -c $SINGLE_QUOTED_CMD" C-m
 	fi
 done
 
